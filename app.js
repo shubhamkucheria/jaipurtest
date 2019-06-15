71349287393e138e801d30b502f6538c199ff837
shubhamkucheria <shubhamkucheria@gmail.com>
2019-06-14 22:57:25 -0400

    connect mongodb

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+// my vars
+const port = process.env.PORT || 3000;
+const {MONGO_URL} = 'mongodb://testjpr:testjpr@testjpr-shard-00-00-mcnbk.mongodb.net:27017,testjpr-shard-00-01-mcnbk.mongodb.net:27017,testjpr-shard-00-02-mcnbk.mongodb.net:27017/testjpr?ssl=true&replicaSet=testjpr-shard-0&authSource=admin&retryWrites=true&w=majority';
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// configuration
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+// static files
+app.set('view engine', 'ejs');
+// routes
+app.use(require('./routes/')); // main routes
+app.use('/auth', require('./routes/user')); // user routes
+// run server
+server.listen(port, () => console.info(`App running on port ${port}`));
