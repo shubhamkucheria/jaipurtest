82950291385921a75cc93c94e36582bdbd31f715
shubhamkucheria <shubhamkucheria@gmail.com>
2019-06-15 06:22:45 -0400

    add new route for vote success

--- routes/user.js
+++ routes/user.js
@@ -4,13 +4,15 @@ const electionController = require('../controllers/electionController');
 const {ensureGuest, ensureAuthenticated} = require('../libs/auth');
 
 /* *** GET ENDPOINTS *** */
-router.post('/vote', ensureGuest, electionController.vote);
-router.post('/partyvote', ensureGuest, electionController.vote);
+router.post('/vote', ensureAuthenticated, electionController.vote);
+router.post('/partyvote', ensureGuest, electionController.partyvote);
 router.get('/login', ensureGuest, electionController.login);
 router.get('/register', ensureGuest, electionController.register);
 router.get('/logout', ensureAuthenticated, electionController.logout);
 router.get('/secret', ensureAuthenticated, electionController.secret);
+router.get('/status', ensureGuest, electionController.status)
 /* *** POST ENDPOINTS *** */
 router.post('/register', electionController.postRegister);
 router.post('/login', electionController.postLogin);
+
 module.exports = router;
