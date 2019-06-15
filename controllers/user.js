290fe9b67e0fcaf4b33ad33864f696d673f11272
shubhamkucheria <shubhamkucheria@gmail.com>
2019-06-15 01:40:31 -0400

    encrypt password using bcryprjs and save into dbs

--- controllers/user.js
+++ controllers/user.js
@@ -1,28 +1,51 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const User = require('../models/User');
+const Vote = require('../models/Vote');
 
 module.exports = {
-  /*  GET ENDPOINTS  */
+  /* *** GET ENDPOINTS *** */
   login: (req, res) => res.render('auth/login'),
   register: (req, res) => res.render('auth/register'),
   logout: (req, res) => {
     req.logout();
     res.redirect('/auth/login');
   },
-  /*  POST ENDPOINTS  */
+  secret: (req, res) => res.render('auth/secret'),
+  /* *** POST ENDPOINTS *** */
   postRegister: (req, res) => {
-   
-    const newUser = new User({
+      User.findOne({email: req.body.email})
+        .then(user => {
+          if (user) {
+            errors.push({text: 'User already exist!'});
+            res.render('auth/register', {errors, name: '', email: '', password: '', rpassword: ''});
+          } else {
+            const newUser = new User({
               name: req.body.name,
               email: req.body.email,
               password: req.body.password,
               voted: false
             });
-           
-      newUser.save()
-        .then(user => {
-          console.log(`User ${user.name} register!`);
-          res.redirect('/auth/login');
-      })
+            bcrypt.genSalt(10, (err, salt) => {
+              bcrypt.hash(newUser.password, salt, (err, hash) => {
+                if (err) throw err;
+                newUser.password = hash;
+                newUser.save()
+                  .then(user => {
+                    console.log(`User ${user.name} register!`);
+                    res.redirect('/auth/login');
+                })
+                .catch(err => console.log(err));
+              });
+            });
+          }
+        });
   },
-  postLogin: (req, res, next) =>  res.redirect('/auth/secret');    
+  postLogin: (req, res, next) => {
+    passport.authenticate('local', {
+     successRedirect: '/auth/secret',
+     failureRedirect: '/auth/login'
+   })(req, res, next);
+ } // Finish
 };
